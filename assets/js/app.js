a8af2b1e15be76101f0bc081a08305682ad72b0a
Mat Hayward <mat@madebykind.com>
2025-03-23 14:00:15 +0000

    remove secret + play video inline

--- assets/js/app.js
+++ assets/js/app.js
@@ -157,9 +157,9 @@ function Video(el) {
     playPause(video);
   });
 
-  window.addEventListener("resize", function() {
+  window.addEventListener("resize",debounce(function(e){
     playPause(video);
-  });
+  }));
 
 }
 
@@ -176,62 +176,14 @@ function ScrollToHash(el){
   }
 }
 
-
-
-// *** SECRETS *** //
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  const expires = `expires=${d.toUTCString()}`;
-  document.cookie = `${cname}=${cvalue};${expires};path=/`;
+// *** DEBOUNCE *** //
+function debounce(func){
+  var timer;
+  return function(event){
+    if(timer) clearTimeout(timer);
+    timer = setTimeout(func,100,event);
+  };
 }
-
-// Read cookie
-function getCookie(cname) {
-  const name = `${cname}=`;
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i += 1) {
-    let c = ca[i];
-    while (c.charAt(0) === " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function showContent(form, elements) {
-  document.body.classList.remove('overflow-hidden');
-  form.remove();
-  elements.forEach((element) => {
-    const removeClasses = element.dataset.secretRemoveClasses.split(" ");
-    const newClasses = element.dataset.secretClasses.split(" ");
-    element.classList.remove(...removeClasses);
-    element.classList.add(...newClasses);
-      setCookie('authenticated', 1, 365);
-  });
-}
-
-
-function Secret(el) {
-  const elements = document.querySelectorAll("[data-secret]");
-  const form = document.querySelector("[data-secret-form]");
-  if (getCookie('authenticated') === "1") {
-      showContent(form, elements);
-  } else {
-    el.addEventListener("keyup", function(e) {
-      if (this.value.toLowerCase() == 'rudirudi22') {
-        showContent(form, elements);
-      }
-    });
-  }
-}
-
-
 
 
 // *** INIT *** //
@@ -243,5 +195,4 @@ window.addEventListener('load', (event) => {
   [...document.querySelectorAll("[data-video]")].map((el) => Video(el));
   [...document.querySelectorAll("[data-gallery]")].map((el) => Gallery(el));
   [...document.querySelectorAll("[data-scroll-to-hash]")].map((el) => ScrollToHash(el));
-  [...document.querySelectorAll("[data-secret-key]")].map((el) => Secret(el));
 });
